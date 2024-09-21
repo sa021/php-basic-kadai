89744409583a82373652bc3c2907d5b08181d66a
Taira <sa.hirao1962@gmail.com>
2024-09-22 01:46:56 +0900

    kadai_005をアップロード

---
+++ kadai_005/score.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>テストの平均点を計算しよう</title>
+</head>
+    <p>
+    <?php
+    // 合計点を出力する
+    echo 80 + 60 + 55 +40 +100 +25 +80 +95 + 30 + 60;
+    // 改行する
+    echo '<br>';
+    echo (80 + 60 + 55 +40 +100 +25 +80 +95 + 30 + 60)/10;
+    // 平均点を出力する
+
+    ?>
+    </p>
+<body>
+    
+</body>
+</html>
