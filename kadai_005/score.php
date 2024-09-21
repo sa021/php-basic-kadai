f4fb34c253f4965cf08a98677b8f6724d1c7d68f
Taira <sa.hirao1962@gmail.com>
2024-09-22 02:12:44 +0900

    kadai_005を修正アップロード

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -7,12 +7,31 @@
 </head>
     <p>
     <?php
-    // 合計点を出力する
-    echo 80 + 60 + 55 +40 +100 +25 +80 +95 + 30 + 60;
+    // 変数に値を代入
+    $score1 = 80;
+    $score2 = 60;
+    $score3 = 55;
+    $score4 = 40;
+    $score5 = 100;
+    $score6 = 25;
+    $score7 = 80;
+    $score8 = 95;
+    $score9 = 30;
+    $score10 = 60;
+
+    $total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10 ;
+
+    // 合計値を出力する
+    echo $total ;
     // 改行する
     echo '<br>';
-    echo (80 + 60 + 55 +40 +100 +25 +80 +95 + 30 + 60)/10;
-    // 平均点を出力する
+    // 平均を出力する
+    echo $total/10;
+
+    
+
+    
+
 
     ?>
     </p>
